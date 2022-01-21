ce73cb19deea52607cd19e68e55ffb9a9d214982
kogi-b <briankogi8@gmail.com>
2022-01-22 02:13:57 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is a MODULE (BY DEFAULT)
+// Modules - Encapsulated code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+
+// sayHi(names.brian)
+// sayHi(names.vinni)
